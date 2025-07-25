50a5a6f630e8de22fabd618c02d9f91098575a32
Thoriso Malope <thorimalope@gmail.com>
2025-07-26 01:48:23 +0200

    carousel slider updated to 4

--- react-portfolio/src/showsCarousel.jsx
+++ react-portfolio/src/showsCarousel.jsx
@@ -10,7 +10,7 @@ export default function ShowsCarousel({ podcasts }) {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
   };
